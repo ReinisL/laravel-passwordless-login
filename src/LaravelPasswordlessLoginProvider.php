fc3ac7e0f624cbfc2a7ba385fdd1a986a548f668
Reinis Lucis <reinis@onthemapmarketing.com>
2022-04-22 15:20:26 +0300

    Add middleware to config

--- src/LaravelPasswordlessLoginProvider.php
+++ src/LaravelPasswordlessLoginProvider.php
@@ -2,23 +2,39 @@
 
 namespace Grosv\LaravelPasswordlessLogin;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class LaravelPasswordlessLoginProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->registerRoutes();
+
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('laravel-passwordless-login.php'),
+                __DIR__.'/../config/laravel-passwordless-login.php' => config_path('laravel-passwordless-login.php'),
             ], 'passwordless-login-config');
         }
     }
 
+    /**
+     * Register the Horizon routes.
+     *
+     * @return void
+     */
+    protected function registerRoutes()
+    {
+        Route::group([
+            'middleware' => config('laravel-passwordless-login.middleware', [HandleAuthenticatedUsers::class]),
+        ], function () {
+            $this->loadRoutesFrom(__DIR__.'/routes.php');
+        });
+    }
+
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-passwordless-login');
+        $this->mergeConfigFrom(__DIR__.'/../config/laravel-passwordless-login.php', 'laravel-passwordless-login');
 
         $this->app->singleton('passwordless-login', function ($app) {
             return new PasswordlessLoginManager();
